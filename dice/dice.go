4dc7089c52e12598a066e5c3c4de4f50e6b5179c
Steve Willoughby <steve@madscience.zone>
2022-06-09 15:31:01 -0700

    added note about die-roll titles

--- dice/dice.go
+++ dice/dice.go
@@ -1708,6 +1708,13 @@ func (d *DieRoller) setNewSpecification(spec string) error {
 // The <title> (which, if given, is separated from the rest of the spec with an
 // equals sign (“=”)) is optional and will be included as a comment in the result
 // list to indicate what the purpose of the die roll was for.
+// Note that this module does not interpret the <title> value further, but by
+// convention two special characters are significant to some clients:
+//   ‖ (U+2016) separates multiple titles in the <title> string
+//   ≡ (U+2261) separates the title text on the left with a color on the right.
+// This means that a <title> string of "monster≡blue‖damage≡red" will display
+// a title for the die roll as two separate title values, "monster" in blue
+// and "damage" in red.
 //
 // <expression> can be anything that can be given as the description string
 // to the New constructor (q.v.). At the end of the spec string there
